1dafaab64ed9009edb5015c8afba30752c145706
Nineswiss <adam@nineswiss.com>
2023-07-22 14:58:05 +1000

    Can Update Schema Document Names

--- admin/src/Views/DashView/DashView.js
+++ admin/src/Views/DashView/DashView.js
@@ -31,9 +31,16 @@ export default function DashView() {
     function readCollection(name) {
         setSchemaName(name)
         const holdCollection = collections.find(item => item.name === name)
-        console.log(holdCollection.schema[0]);
-        setSchema(holdCollection.schema[0])
-        console.log(holdCollection.schema[1].timestamps);
+        const schema = holdCollection.schema[0]
+        const updatedObject = {};
+        Object.keys(schema).forEach((key, index) => {
+          const item = schema[key];
+          updatedObject[key] = {
+            ...item,
+            id: uuidv4() 
+          };
+        });
+        setSchema(updatedObject)
         setTimestamp(holdCollection.schema[1].timestamps.toString())
     }
 
@@ -49,13 +56,52 @@ export default function DashView() {
         tempSchema[name].type=value
         setSchema(tempSchema)
     }
-    function updateName(name){
 
+    function updateName(e,id){
+
+        let tempSchema = {...schema}
+        const updatedKey = e.target.value;
+        const entryIndex = findEntryIndexById(tempSchema, id);
+
+        const entries = Object.entries(tempSchema);
+        const [key, value] = entries[entryIndex];
+        const updatedEntries = [
+            ...entries.slice(0, entryIndex),
+            [updatedKey, value],
+            ...entries.slice(entryIndex + 1)
+        ];
+        const updatedObject = Object.fromEntries(updatedEntries);
+
+        setSchema(updatedObject)
+    }
+
+    const findEntryIndexById = (obj, targetId) => {
+        const entries = Object.entries(obj);
+        for (let i = 0; i < entries.length; i++) {
+          const [key, value] = entries[i];
+          if (value.id === targetId) {
+            return i; // Return the index of the matching entry
+          }
+        }
+        return -1; // If no matching entry is found
+      };
+
+
+    function updateDefault(e,name){
+        let tempSchema = {...schema}
+        tempSchema[name].default = e.target.value
+        setSchema(tempSchema)
     }
 
     async function save(){
+        const updatedObject = {};
+        Object.keys(schema).forEach(key => {
+          const { id, ...itemWithoutId } = schema[key];
+          updatedObject[key] = itemWithoutId;
+        });
+
         let finalModel=[
-            schema,
+            updatedObject,
             {
                 "timestamps": timestamp
             }
@@ -65,7 +111,13 @@ export default function DashView() {
     }
 
     function addRow(){
-        console.log("Add");
+        let newName = prompt("Name")
+        if(!newName){return}
+        let exists = schema[newName]
+        if(exists){
+            alert("Name must be unique")
+            return
+        }
         let tempSchema = {...schema}
         const newRow = {
             name:{
@@ -73,12 +125,19 @@ export default function DashView() {
             required:false,
             default:''
         }}
-        const newName = uuidv4()
         newRow[newName] = newRow.name;
         delete newRow.name;
-        console.log(newRow);
         Object.assign(tempSchema,newRow)
         console.log(tempSchema);
+        setSchema(tempSchema)
+    }
+
+    function deleteRow(name){
+        let ask = window.confirm("Are you sure you wnat to delete: " + name)
+        if(!ask){return}
+
+        let tempSchema = {...schema}
+        delete tempSchema[name]
         setSchema(tempSchema)
     }
 
@@ -117,9 +176,9 @@ export default function DashView() {
                 <tbody>
                     { Object.entries(schema).map(([property, attributes],index) => (
                        
-                        <tr className="schemaRow" key={'dec'+index}>
+                        <tr className="schemaRow" key={attributes.id}>
                             <td>
-                                <input defaultValue={property} onChange={updateName()}/>
+                                <input defaultValue={property} onChange={(e)=>updateName(e,attributes.id)} type="text"/>
                             </td>
                             <td>
                             <select value={attributes.type} onChange={(e)=>updateType(e.target.value,property)}>
@@ -138,9 +197,9 @@ export default function DashView() {
                                 <option value="BigInt">BigInt</option>
                             </select>
                             </td>
-                            <td><input type="checkbox" checked={attributes.required} onChange={()=>updateReq(property)}/></td>
-                            <td>{attributes.default}</td>
-                            <td><button>Delete</button></td>
+                            <td><input type="checkbox" checked={attributes.required ? attributes.required : false} onChange={()=>updateReq(property)}/></td>
+                            <td> <input defaultValue={attributes.default} onChange={(e)=>updateDefault(e,property)}  type="text"/></td>
+                            <td><button onClick={()=>deleteRow(property)}>Delete</button></td>
                         </tr>
                         
                    ))}
